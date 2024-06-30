137f6aad80f3f941b523ffc41d419a1c88f07961
Chitsanupong <iloveheapsort@gmail.com>
2024-06-30 15:03:23 +0700

    connect backend with the frontend

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,30 @@
+<script lang="ts">
+	import { createMutation, useQueryClient } from '@tanstack/svelte-query';
+	import Client, { yume, Local } from '../client';
+	const encoreClient = new Client(Local);
+	const queryClient = useQueryClient();
+	let mutation = createMutation({
+		mutationFn: (params: yume.NewDocumentParams) => encoreClient.yume.NewDocument(params),
+		onSuccess: (data) => {
+			console.log(data);
+		},
+		onError: (error) => {
+			console.error(error);
+		}
+		// TODO: Invalidate shit
+	});
+	let userid: string,
+		content: string,
+		name: string = '';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<input bind:value={userid} placeholder="user id" />
+<input bind:value={name} placeholder="document name" />
+<input bind:value={content} placeholder="content" />
+
+<button
+	on:click={() => $mutation.mutate({ OWNER: userid, NAME: name, CONTENT_BASE64: btoa(content) })}
+	>test</button
+>
